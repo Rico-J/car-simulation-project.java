8caf159bd9446630de234b3e875cb0cb980a19be
Joel Rico <167577518+Rico-J@users.noreply.github.com>
2025-05-01 13:09:58 -0700

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Car myFirstCar = new Car();
+
+        myFirstCar.make = "Toyota";
+        myFirstCar.model = "GR86";
+        myFirstCar.color = "Black";
+        myFirstCar.year = 2019;
+
+        myFirstCar.getCarDetails();
+        // array of Car objects
+        Car[] cars = new Car[5];
+
+        Scanner scanner = new Scanner(System.in);
+
+        for (int index = 0; index < 5; index++){
+            cars[index] = new Car();
+            System.out.println("Enter details for car " +  (index + 1));
+
+            System.out.println("Make: ");
+            cars[index].make = scanner.nextLine();
+
+            System.out.println("Model: ");
+            cars[index].model = scanner.nextLine();
+
+            System.out.println("Color: ");
+            cars[index].color = scanner.nextLine();
+
+            System.out.println("Year: ");
+            cars[index].year = scanner.nextInt();
+            scanner.nextLine();
+
+
+        }
+        for (int index = 0; index < 5; index++){
+            String carDetails = cars[index].getCarDetails();
+            System.out.println(carDetails);
+        }
+    }
+}
